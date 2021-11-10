0a22c3b6a61feefdacfe7528845efdab7300adf3
xiaoxu <2932001008@qq.com>
2021-11-10 11:02:57 +0800

    first

--- app/src/main/java/com/bawei/xuwenhao_util/StringUtils.java
+++ app/src/main/java/com/bawei/xuwenhao_util/StringUtils.java
@@ -8,6 +8,7 @@ package com.bawei.xuwenhao_util;
  * Time: 10:53
  */
 public class StringUtils {
+    //判断字符串
     public static boolean isEmpty(String string){
         if (string.trim().equals("")||null==string){
             return true;
